9afe5d80aebbffb824bf66d4ea8e77d169aa663f
egidum <sixpilot80@yahoo.com>
2019-08-09 22:02:24 +0300

    paskaitos

--- Frontend/11 paskaita/dinaminis html/js/gallery.js
+++ Frontend/11 paskaita/dinaminis html/js/gallery.js
@@ -4,14 +4,36 @@
 function startGallery(){				document.querySelector(".modal").addEventListener("click", function(){
 		this.style.display = "none";
 	});
+	drawGallery();
+	
+	let currentImageId = 0;
+	document.querySelector("#right").addEventListener("click", function(event){
+		event.stopPropagation();
+		
+		let imageInModal = document.querySelector(".modal .center img");
+		currentImageId = imageInModal.getAttribute("id");
+		
+		currentImageId++;
+		let newImage = document.getElementById(currentImageId);
+		document.querySelector(".modal .center img").setAttribute("src", newImage.getAttribute("src"));
+		document.querySelector(".modal .center img").setAttribute("id", newImage.getAttribute("id"));
+		
+		});
+						
+}
+
+function drawGallery(){
+	let allImages = getGallery();
+	for(let i = 0; i < allImages.length; i++){
+		createImage(allImages[i].imageLink, allImages[i].id, allImages[i].title);
+	}
 	
 }
 
 
-
 /*Automatinis div ir img sukurimas*/
 
-function createImage(imageLink, id){
+function createImage(imageLink, id, title){
 	let divMain = document.createElement("div");
 		divMain.setAttribute("class", "coll4");
 		
@@ -22,14 +44,18 @@ function createImage(imageLink, id){
 		let image = document.createElement("img");
 		image.setAttribute("src", imageLink);
 		image.setAttribute("id", id);
+		image.setAttribute("title", title);
 		divImage.appendChild(image);
 		
 		image.addEventListener("click", function(){
 			let image = document.createElement("img");
 			image.setAttribute("src", this.getAttribute("src"));
+			image.setAttribute("id", this.getAttribute("id"));
 			document.querySelector(".modal .center").appendChild(image);
 			document.querySelector(".modal").style.display = "flex";				   
    		});
+	
+	//iterpiam paveikslelio nuoroda
 //	
 //		let image = document.createElement("img");
 //		image.setAttribute("src", "https://picsum.photos/300");
